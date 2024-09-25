3793a5474d41c2bb043488d7b8fc8dca9193eb4a
bbulatao <bbulatao@stevens.edu>
2024-09-25 00:43:53 -0400

    socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,46 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
-    <>
-      <div className="socials text-lg text-serika_main p-4">Socials</div>
-    </>
+    <footer className="bg-serika_bg text-serika_main p-4">
+      <div className="socials text-lg text-center">
+        <h3 className="mb-2">Connect with me:</h3>
+        <div className="flex justify-center space-x-4">
+          {/* LinkedIn */}
+          <a
+            href="https://www.linkedin.com/in/branden-bulatao-4a4851251/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-700"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+
+          {/* GitHub */}
+          <a
+            href="https://github.com/BulaCooola "
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-gray-700"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+
+          {/* Photography Portfolio */}
+          <a
+            href="https://bbulataophoto.myportfolio.com/home"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-blue-500"
+          >
+            <FontAwesomeIcon icon={faCamera} size="2x" />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 }
 
